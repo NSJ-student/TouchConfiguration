8726a79461350a6780fd01c0e0a1552307e93ec0
NSJ-student <ghdlghdl0558@gmail.com>
2018-11-27 02:55:24 +0900

    터치 configuration 번역 기능 완성

--- touchConfig/Form1.Designer.cs
+++ touchConfig/Form1.Designer.cs
@@ -28,13 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tabResult = new System.Windows.Forms.TabControl();
+            this.scrollTab = new System.Windows.Forms.HScrollBar();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.625F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 87.375F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 92F));
+            this.tableLayoutPanel1.Controls.Add(this.btnOpen, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.progressBar1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnSave, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tabResult, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.scrollTab, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 17F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnOpen.Location = new System.Drawing.Point(3, 3);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(83, 32);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(92, 3);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(612, 32);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(710, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(87, 32);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // tabResult
+            // 
+            this.tabResult.Alignment = System.Windows.Forms.TabAlignment.Bottom;
+            this.tableLayoutPanel1.SetColumnSpan(this.tabResult, 3);
+            this.tabResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabResult.Location = new System.Drawing.Point(3, 41);
+            this.tabResult.Name = "tabResult";
+            this.tabResult.SelectedIndex = 0;
+            this.tabResult.Size = new System.Drawing.Size(794, 389);
+            this.tabResult.TabIndex = 3;
+            // 
+            // scrollTab
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.scrollTab, 3);
+            this.scrollTab.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scrollTab.LargeChange = 1;
+            this.scrollTab.Location = new System.Drawing.Point(0, 433);
+            this.scrollTab.Name = "scrollTab";
+            this.scrollTab.Size = new System.Drawing.Size(800, 17);
+            this.scrollTab.TabIndex = 4;
+            this.scrollTab.ValueChanged += new System.EventHandler(this.hScrollBar1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Touch Configuration";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TabControl tabResult;
+        private System.Windows.Forms.HScrollBar scrollTab;
     }
 }
 
--- touchConfig/Form1.cs
+++ touchConfig/Form1.cs
@@ -7,14 +7,233 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace touchConfig
 {
     public partial class Form1 : Form
     {
+        BackgroundWorker worker;
+
         public Form1()
         {
             InitializeComponent();
+
+            worker = new BackgroundWorker();
+
+            worker.DoWork += DoInterprete;
+            worker.ProgressChanged += progressChanged;
+            worker.RunWorkerCompleted += RunCompleted;
+            worker.WorkerReportsProgress = true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Filter = "xcfg files (*.xcfg) | *.xcfg";
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                long length = new System.IO.FileInfo(dialog.FileName).Length;
+                progressBar1.Maximum = (int)length;
+                progressBar1.Value = 0;
+                scrollTab.Maximum = 0;
+                scrollTab.Minimum = 0;
+                scrollTab.Value = 0;
+
+                worker.RunWorkerAsync(dialog.FileName);
+            }
+        }
+
+        private void DoInterprete(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            string path = (string)e.Argument;
+            
+            var fileStream = File.OpenRead(path);
+            var streamReader = new StreamReader(fileStream, Encoding.UTF8);
+            
+            String line;
+            int current_read = 0;
+            int obj_size = 0;
+            int size_cnt = 0;
+            TabElement tempObj = null;
+            string tabName;
+            bool start = false;
+
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                if (line.Length == 0) continue;
+                
+                current_read = (int)streamReader.BaseStream.Position;
+                try
+                {
+                    if (line.ElementAt<char>(0) == '[')
+                    {
+                        start = false;
+                        string[] spt = line.Split('[', ']', ' ', '_');
+                        if(spt.Length >= 3)
+                        {
+                            for(int cnt=0; cnt<spt.Length; cnt++)
+                            {
+                                if(spt[cnt].Equals("INSTANCE"))
+                                {
+                                    tabName = spt[cnt-1] + "_" + spt[cnt + 1];
+                                    tempObj = new TabElement(tabName);
+                                    start = true;
+                                }
+                            }
+                        }
+                        worker.ReportProgress(current_read, null);
+                    }
+                    else if (start)
+                    {
+                        if (tempObj != null)
+                        {
+                            if ((line.Length > 14) && (line.Substring(0, 14).Equals("OBJECT_ADDRESS")))
+                            {
+                                tempObj.AppendText(line);
+                                worker.ReportProgress(current_read, null);
+                            }
+                            else if ((line.Length > 11) && (line.Substring(0, 11).Equals("OBJECT_SIZE")))
+                            {
+                                tempObj.AppendText(line);
+                                obj_size = Convert.ToInt32(line.Split('=')[1]);
+                                size_cnt = 0;
+                                worker.ReportProgress(current_read, null);
+                            }
+                            else
+                            {
+                                string[] datas = line.Split(' ', '=');
+
+                                Int32 length = Convert.ToInt32(datas[1]);
+                                Int32 data = Convert.ToInt32(datas[3]);
+                                size_cnt += length;
+
+                                byte[] bytes = BitConverter.GetBytes(data);
+
+                                for (Int32 cnt = 0; cnt < length; cnt++)
+                                {
+                                    tempObj.AppendText(bytes[cnt].ToString("X2"));
+                                }
+
+                                if (size_cnt == obj_size)
+                                {
+                                    worker.ReportProgress(current_read, tempObj);
+                                }
+                                else
+                                {
+                                    worker.ReportProgress(current_read, null);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        worker.ReportProgress(current_read, null);
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("err22");
+                }
+            }
+
+            worker.ReportProgress((int)streamReader.BaseStream.Position, null);
+            streamReader.Close();
+            fileStream.Close();
+        }
+
+        private void progressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            if(e.UserState != null)
+            {
+                try
+                {
+                    TabElement element = (TabElement)e.UserState;
+                    RichTextBox rtbTemp = new RichTextBox();
+                    if (element.strList.Count > 0)
+                    {
+                        string[] strArr = element.strList.ToArray();
+                        rtbTemp.AppendText(strArr[0]);
+                        rtbTemp.AppendText("\r\n");
+                        rtbTemp.AppendText(strArr[1]);
+                        rtbTemp.AppendText("\r\n");
+
+                        int start_idx = 2;
+                        
+                        while(start_idx + 16 < strArr.Length-1)
+                        {
+                            rtbTemp.AppendText(String.Join(" ", strArr, start_idx, 16));
+                            rtbTemp.AppendText("\r\n");
+                            start_idx += 16;
+                        }
+
+                        int rest = strArr.Length - start_idx;
+
+                        if(rest > 0)
+                        {
+                            rtbTemp.AppendText(String.Join(" ", strArr, start_idx, rest));
+                        }
+                    }
+                    tabResult.TabPages.Add(element.tabName, element.tabName);
+                    tabResult.TabPages[element.tabName].Controls.Add(rtbTemp);
+                    rtbTemp.Dock = DockStyle.Fill;
+
+                    scrollTab.Maximum = tabResult.TabPages.Count;
+                }
+                catch
+                {
+                    MessageBox.Show("err");
+                }
+            }
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void RunCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                MessageBox.Show("Canceled");
+            }
+            else if (e.Error != null)
+            {
+                MessageBox.Show("Error: " + e.Error.Message);
+            }
+            else
+            {
+                MessageBox.Show("complete");
+            }
+        }
+
+        private void hScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            if(tabResult.TabPages.Count > scrollTab.Value)
+            {
+                tabResult.SelectTab(scrollTab.Value);
+            }
         }
     }
+
+    class TabElement
+    {
+        public string tabName;
+        public List<string> strList;
+
+        public TabElement(string name)
+        {
+            tabName = name;
+            strList = new List<string>();
+        }
+
+        public void AppendText(string text)
+        {
+            strList.Add(text);
+        }
+    }
+
 }
